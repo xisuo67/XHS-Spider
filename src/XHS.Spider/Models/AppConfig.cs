acfc200778c434285481ad3b946e72447de6995c
初十 <392706283@qq.com>
2023-04-25 14:42:14 +0800

    首次框架提交 wpf-ui

---
+++ src/XHS.Spider/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace XHS.Spider.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
---
+++ src/XHS.Spider/Models/DataColor.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+
+namespace XHS.Spider.Models
+{
+    public struct DataColor
+    {
+        public Brush Color { get; set; }
+    }
+}
