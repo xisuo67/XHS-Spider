527e1a0888f355e52617e91bc0c57e6c9a2cf2bc
初十 <392706283@qq.com>
2023-04-27 17:03:35 +0800

    增加获取笔记详情接口

--- src/XHS.IService/XHS/IXhsSpiderService.cs
+++ src/XHS.IService/XHS/IXhsSpiderService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XHS.Models.XHS.ApiOutputModel.NodeDetail;
 using XHS.Models.XHS.ApiOutputModel.UserPosted;
 using XHS.Models.XHS.InputModel;
 
@@ -19,5 +20,13 @@ namespace XHS.IService.XHS
         /// <param name="model"></param>
         /// <returns></returns>
         UserPostedModel UserPosted(UserPostedInputModel model);
+
+        /// <summary>
+        /// 获取用户笔记详情
+        /// /api/sns/web/v1/feed?source_note_id=xxxxx
+        /// </summary>
+        /// <param name="nodeid"></param>
+        /// <returns></returns>
+        NodeDetailModel GetNodeDetail(string nodeid);
     }
 }
--- src/XHS.Service/XHS/XhsSpiderService.cs
+++ src/XHS.Service/XHS/XhsSpiderService.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using XHS.Common.Http;
 using XHS.IService.XHS;
+using XHS.Models.XHS.ApiOutputModel.NodeDetail;
 using XHS.Models.XHS.ApiOutputModel.UserPosted;
 using XHS.Models.XHS.InputModel;
 
@@ -16,6 +17,30 @@ namespace XHS.Service.XHS
     /// </summary>
     public class XhsSpiderService : IXhsSpiderService
     {
+        /// <summary>
+        /// 获取笔记详情
+        /// </summary>
+        /// <param name="nodeid"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public NodeDetailModel GetNodeDetail(string nodeid)
+        {
+            NodeDetailModel nodeDetailModel = new NodeDetailModel();
+            try
+            {
+                string url = $"/api/sns/web/v1/feed?source_note_id={nodeid}";
+                var result = HttpClientHelper.DoPost(url);
+                if (!string.IsNullOrEmpty(result))
+                {
+                    nodeDetailModel = JsonConvert.DeserializeObject<NodeDetailModel>(result);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return nodeDetailModel;
+        }
+
         /// <summary>
         /// 分页查询用户所有笔记接口
         /// TODO:这里后面迭代要改掉，先测试接口是否能调用成功
@@ -45,5 +70,7 @@ namespace XHS.Service.XHS
             }
             return userPostedModel;
         }
+
+
     }
 }
