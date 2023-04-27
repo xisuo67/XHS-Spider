71b74974800c298ac08d5b50c755d218f1bfdbe0
初十 <392706283@qq.com>
2023-04-27 17:59:59 +0800

    增加小红书接口

--- src/XHS.IService/XHS/IXhsSpiderService.cs
+++ src/XHS.IService/XHS/IXhsSpiderService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using XHS.Models.XHS.ApiOutputModel.NodeDetail;
+using XHS.Models.XHS.ApiOutputModel.OtherInfo;
 using XHS.Models.XHS.ApiOutputModel.UserPosted;
 using XHS.Models.XHS.InputModel;
 
@@ -28,5 +29,12 @@ namespace XHS.IService.XHS
         /// <param name="nodeid"></param>
         /// <returns></returns>
         NodeDetailModel GetNodeDetail(string nodeid);
+
+        /// <summary>
+        /// 获取当前小红书博主其他信息，包含个人信息等数据
+        /// </summary>
+        /// <param name="targetUserId"></param>
+        /// <returns></returns>
+        OtherInfoModel GetOtherInfo(string targetUserId);
     }
 }
--- src/XHS.Service/XHS/XhsSpiderService.cs
+++ src/XHS.Service/XHS/XhsSpiderService.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 using XHS.Common.Http;
 using XHS.IService.XHS;
 using XHS.Models.XHS.ApiOutputModel.NodeDetail;
+using XHS.Models.XHS.ApiOutputModel.OtherInfo;
 using XHS.Models.XHS.ApiOutputModel.UserPosted;
 using XHS.Models.XHS.InputModel;
+using static Microsoft.ClearScript.V8.V8CpuProfile;
 
 namespace XHS.Service.XHS
 {
@@ -40,6 +42,29 @@ namespace XHS.Service.XHS
             }
             return nodeDetailModel;
         }
+        /// <summary>
+        /// 获取当前小红书博主其他信息，包含个人信息等数据
+        /// </summary>
+        /// <param name="targetUserId"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public OtherInfoModel GetOtherInfo(string targetUserId)
+        {
+            OtherInfoModel model=new OtherInfoModel();
+            try
+            {
+                string url = $"/api/sns/web/v1/user/otherinfo?target_user_id={targetUserId}";
+                var result = HttpClientHelper.DoGet(url);
+                if (!string.IsNullOrEmpty(result))
+                {
+                    model = JsonConvert.DeserializeObject<OtherInfoModel>(result);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return model;
+        }
 
         /// <summary>
         /// 分页查询用户所有笔记接口
@@ -57,7 +82,7 @@ namespace XHS.Service.XHS
                 try
                 {
                     string url = $"/api/sns/web/v1/user_posted?num={model.num}&cursor=&user_id={model.user_id}";
-                    var result = HttpClientHelper.DoPost(url);
+                    var result = HttpClientHelper.DoGet(url);
                     if (!string.IsNullOrEmpty(result))
                     {
                         userPostedModel = JsonConvert.DeserializeObject<UserPostedModel>(result);
