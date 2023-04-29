41f0f09967a73b0172f286aaf073b0e38c67a889
初十 <392706283@qq.com>
2023-04-29 21:47:18 +0800

    代码调整

--- src/XHS.Spider/Services/SearchService.cs
+++ src/XHS.Spider/Services/SearchService.cs
@@ -16,9 +16,59 @@ namespace XHS.Spider.Services
         /// <exception cref="NotImplementedException"></exception>
         public static void SearchInput(string input, INavigation navigation)
         {
-            //测试跳转
-            navigation.Navigate(typeof(Views.Pages.UserProfilePage), "test");
-           
+            if (input.Contains("user/profile"))
+            {
+                //测试跳转
+                navigation.Navigate(typeof(Views.Pages.UserProfilePage), "test");
+            }
+        }
+
+        /// <summary>
+        /// 从url中获取id
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="baseUrl"></param>
+        /// <returns></returns>
+        public static string GetId(string input, string baseUrl)
+        {
+            if (!IsUrl(input)) { return ""; }
+
+            string url = EnableHttps(input);
+            url = DeleteUrlParam(url);
+            return url.Replace(baseUrl, "");
+        }
+
+        /// <summary>
+        /// 是否为网址
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static bool IsUrl(string input)
+        {
+            return input.StartsWith("http://") || input.StartsWith("https://");
+        }
+
+        /// <summary>
+        /// 将http转为https
+        /// </summary>
+        /// <returns></returns>
+        private static string EnableHttps(string url)
+        {
+            if (!IsUrl(url)) { return null; }
+
+            return url.Replace("http://", "https://");
+        }
+
+        /// <summary>
+        /// 去除url中的参数
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private static string DeleteUrlParam(string url)
+        {
+            string[] strList = url.Split('?');
+
+            return strList[0].EndsWith("/") ? strList[0].TrimEnd('/') : strList[0];
         }
     }
 }
--- src/XHS.Spider/ViewModels/UserProfileViewModel.cs
+++ src/XHS.Spider/ViewModels/UserProfileViewModel.cs
@@ -3,21 +3,34 @@ using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
+using XHS.IService.XHS;
+using XHS.Spider.Services;
 
 namespace XHS.Spider.ViewModels
 {
     public partial class UserProfileViewModel : ObservableObject, INavigationAware
     {
+        public static readonly string BaseUrl = "https://www.xiaohongshu.com/user/profile/";
         private string inputText;
         public string InputText
         {
             get => inputText;
             set => SetProperty(ref inputText, value);
         }
+        private readonly IXhsSpiderService _xhsSpiderService;
+        private readonly INavigationService _navigationService;
+        public UserProfileViewModel(INavigationService navigationService, IXhsSpiderService xhsSpiderService)
+        {
+            _xhsSpiderService = xhsSpiderService;
+        }
+
+
         // 输入确认事件
         private ICommand inputCommand;
         public ICommand InputCommand
@@ -30,17 +43,30 @@ namespace XHS.Spider.ViewModels
         /// </summary>
         private void ExecuteInput()
         {
+            if (!string.IsNullOrEmpty(InputText))
+            {
+                if (InputText.Contains("user/profile/"))
+                {
+                   var id= SearchService.GetId(inputText,BaseUrl);
+                    if (string.IsNullOrEmpty(id)) {
+                        return;
+                    }
+                    else
+                    {
+                        var userinfo= _xhsSpiderService.GetOtherInfo(id);
+                    }
+                }
+            }
             //TODO:
         }
 
         public void OnNavigatedFrom()
         {
-            
         }
 
         public void OnNavigatedTo()
         {
-            
+
         }
     }
 }
