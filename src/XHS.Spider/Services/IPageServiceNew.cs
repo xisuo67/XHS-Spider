65a70b3ea839a5da306182381f824359ce1a9bb5
初十 <392706283@qq.com>
2023-04-30 18:13:30 +0800

    增加IPageServiceNew

---
+++ src/XHS.Spider/Services/IPageServiceNew.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Ui.Mvvm.Contracts;
+
+namespace XHS.Spider.Services
+{
+    public interface IPageServiceNew: IPageService
+    {
+        // 这个用做特定的服务范围
+        IServiceScope? Scope { get; set; }
+    }
+}
--- src/XHS.Spider/Services/PageService.cs
+++ src/XHS.Spider/Services/PageService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows;
 using Wpf.Ui.Mvvm.Contracts;
 
@@ -7,7 +8,7 @@ namespace XHS.Spider.Services
     /// <summary>
     /// Service that provides pages for navigation.
     /// </summary>
-    public class PageService : IPageService
+    public class PageService : IPageServiceNew, IDisposable
     {
         /// <summary>
         /// Service which provides the instances of pages.
@@ -20,6 +21,13 @@ namespace XHS.Spider.Services
         public PageService(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
+        }
+        IServiceScope? m_scope = null;
+        public IServiceScope? Scope { get => m_scope; set => m_scope = value; }
+
+        public void Dispose()
+        {
+            m_scope?.Dispose();
         }
 
         /// <inheritdoc />
@@ -36,6 +44,18 @@ namespace XHS.Spider.Services
         {
             if (!typeof(FrameworkElement).IsAssignableFrom(pageType))
                 throw new InvalidOperationException("The page should be a WPF control.");
+
+            // 在进入创建YourScopedPage之前，检查scope是否存在
+            if (pageType == typeof(Views.Pages.UserProfilePage) && m_scope is not null)
+            {
+                // 如果存在，那就在这个scope内找到这个page类型的实例，如果没有就创建
+                return m_scope.ServiceProvider.GetRequiredService(pageType) as FrameworkElement;
+            }
+            else
+            {
+                m_scope?.Dispose();
+                m_scope = null;
+            }
 
             return _serviceProvider.GetService(pageType) as FrameworkElement;
         }
