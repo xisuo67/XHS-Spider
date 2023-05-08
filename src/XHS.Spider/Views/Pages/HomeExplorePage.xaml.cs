b5208f093e092e5b83ddadbbf1def81875a82ed2
初十 <392706283@qq.com>
2023-05-08 18:05:46 +0800

    代码调整
    代码调整

--- src/XHS.Spider/Views/Pages/HomeExplorePage.xaml.cs
+++ src/XHS.Spider/Views/Pages/HomeExplorePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Common.Interfaces;
 
 namespace XHS.Spider.Views.Pages
 {
