cbbabb1737c784fa6dddebb42dfaeb95c08cb53d
初十 <392706283@qq.com>
2023-05-24 20:35:12 +0800

    实体代码提交

---
+++ src/XHS.Models/XHS/ApiOutputModel/Search/SearchOutPutModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHS.Models.XHS.ApiOutputModel.Search
+{
+    public class SearchOutPutModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("word_request_id")]
+        public string WordRequestId { get; set; }
+        [JsonProperty("filter_tags")]
+        public List<FilterTag> FilterTags { get; set; }
+    }
+}
