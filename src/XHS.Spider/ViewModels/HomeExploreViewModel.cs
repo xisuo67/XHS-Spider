9bbbcbd91e7eb0d2876cce1408e65fdd975091a9
初十 <392706283@qq.com>
2023-05-24 13:38:10 +0800

    关键字搜索页面开发

--- src/XHS.Spider/ViewModels/HomeExploreViewModel.cs
+++ src/XHS.Spider/ViewModels/HomeExploreViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Wpf.Ui.Common.Interfaces;
 
 namespace XHS.Spider.ViewModels
@@ -13,6 +14,31 @@ namespace XHS.Spider.ViewModels
     /// </summary>
     public class HomeExploreViewModel : ObservableObject, INavigationAware
     {
+        #region 属性
+        private string inputText;
+        public string InputText
+        {
+            get => inputText;
+            set => SetProperty(ref inputText, value);
+        }
+        // 输入确认事件
+        private ICommand inputCommand;
+        public ICommand InputCommand
+        {
+            get => inputCommand ?? (inputCommand = new Wpf.Ui.Common.RelayCommand(ExecuteInitData));
+            set => inputCommand = value;
+        }
+        #endregion
+
+        #region 搜索
+        /// <summary>
+        /// 处理输入事件
+        /// </summary>
+        public async void ExecuteInitData()
+        {
+        
+        }
+        #endregion
         public void OnNavigatedFrom()
         {
         }
