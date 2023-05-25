db931d8da514d361b30da016bd5ea6549740290c
初十 <392706283@qq.com>
2023-05-25 22:13:06 +0800

    代码提交

--- src/XHS.Spider/ViewModels/HomeExploreViewModel.cs
+++ src/XHS.Spider/ViewModels/HomeExploreViewModel.cs
@@ -23,9 +23,10 @@ namespace XHS.Spider.ViewModels
             get => inputText;
             set => SetProperty(ref inputText, value);
         }
-        private ObservableCollection<SearchNodesModel> _nodes =new ObservableCollection<SearchNodesModel>();
+        private ObservableCollection<SearchNodesModel> _nodes = new ObservableCollection<SearchNodesModel>();
 
-        public ObservableCollection<SearchNodesModel> Nodes {
+        public ObservableCollection<SearchNodesModel> Nodes
+        {
             get { return _nodes; }
             set => SetProperty(ref _nodes, value);
         }
@@ -45,7 +46,26 @@ namespace XHS.Spider.ViewModels
         /// </summary>
         public async void ExecuteInitData()
         {
-        
+            //SearchNodesModel searchNodesModel = new SearchNodesModel()
+            //    {
+            //        Page
+            //    };
+            List<SearchNode> nodes = new List<SearchNode>();
+            for (int i = 0; i < 20; i++)
+            {
+                SearchNode searchNode = new SearchNode()
+                {
+                    avatar= "https://sns-avatar-qc.xhscdn.com/avatar/640ee7a7e64abc0b310374b2.jpg?imageView2/2/w/80/format/jpg",
+                    coverUrl= "https://sns-img-qc.xhscdn.com/1000g008271vlc9ifm0005ovens0jq7qrqb6a3bg?imageView2",
+                    display_title= "春天的海边的微风🌊配上爱如火🔥",
+                    liked=false,
+                    liked_count=800,
+                    nickname= "橙北北"+i,
+                    NodeId= "64102e0d000000000800fa8e",
+                    user_id= "63eebf01000000000f011f5b",
+                };
+                nodes.Add(searchNode);
+            }
         }
         #endregion
         public void OnNavigatedFrom()
