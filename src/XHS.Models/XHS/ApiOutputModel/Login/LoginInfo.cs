28a5e0bb42826b6cdede8b34e85d0d8ffa119668
初十 <392706283@qq.com>
2023-06-03 09:36:38 +0800

    实体代码提交

---
+++ src/XHS.Models/XHS/ApiOutputModel/Login/LoginInfo.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHS.Models.XHS.ApiOutputModel.Login
+{
+    public class LoginInfo
+    {
+        [JsonProperty("secure_session")]
+        public string SecureSession { get; set; }
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+        [JsonProperty("session")]
+        public string Session { get; set; }
+    }
+}
