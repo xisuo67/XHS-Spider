a2b1dc05d27c0126eda0fc7d82cf81249d61b7dd
初十 <392706283@qq.com>
2023-06-05 10:53:50 +0800

    单条笔记ui界面

---
+++ src/XHS.Spider/ViewModels/NodeDetailViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Ui.Common.Interfaces;
+
+namespace XHS.Spider.ViewModels
+{
+    public class NodeDetailViewModel : BaseSearchViewModel, INavigationAware
+    {
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        public override void ExecuteInitData()
+        {
+        }
+
+        public void OnNavigatedFrom()
+        {
+        }
+
+        public void OnNavigatedTo()
+        {
+        }
+    }
+}
