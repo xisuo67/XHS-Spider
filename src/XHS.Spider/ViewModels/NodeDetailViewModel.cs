876d08db2254a6e3431ee44335d20c16eb99d4e7
初十 <392706283@qq.com>
2023-06-05 15:09:53 +0800

    代码调整

--- src/XHS.Spider/ViewModels/NodeDetailViewModel.cs
+++ src/XHS.Spider/ViewModels/NodeDetailViewModel.cs
@@ -6,15 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using Wpf.Ui.Common;
 using Wpf.Ui.Common.Interfaces;
 using Wpf.Ui.Mvvm.Contracts;
+using XHS.Common.Helpers;
 using XHS.Common.Utils;
 using XHS.IService.XHS;
 using XHS.Models.DownLoad;
 using XHS.Models.XHS.ApiOutputModel.OtherInfo;
 using XHS.Models.XHS.ApiOutputModel.UserPosted;
+using XHS.Service.Log;
 using XHS.Spider.Helpers;
+using XHS.Spider.Services;
 
 namespace XHS.Spider.ViewModels
 {
@@ -22,6 +26,7 @@ namespace XHS.Spider.ViewModels
     {
 
         #region 页面属性
+        private static readonly Service.Log.ILogger Logger = LoggerService.Get(typeof(NodeDetailViewModel));
         public static readonly string BaseUrl = "https://www.xiaohongshu.com/user/profile/";
         public static readonly string BaseVideoUrl = "http://sns-video-bd.xhscdn.com/{0}";
         public static readonly string BaseImageUrl = "https://sns-img-bd.xhscdn.com/{0}?imageView2/format/png";
@@ -52,22 +57,22 @@ namespace XHS.Spider.ViewModels
             get => _noteCount;
             set => SetProperty(ref _noteCount, value);
         }
-        /// <summary>
-        /// 解析数量
-        /// </summary>
-        private string _parseNodeCount = "已解析(0)条";
-        public string ParseNodeCount
-        {
-            get => _parseNodeCount;
-            set => SetProperty(ref _parseNodeCount, value);
-        }
-        private ICommand parseNode;
+        ///// <summary>
+        ///// 解析数量
+        ///// </summary>
+        //private string _parseNodeCount = "已解析(0)条";
+        //public string ParseNodeCount
+        //{
+        //    get => _parseNodeCount;
+        //    set => SetProperty(ref _parseNodeCount, value);
+        //}
+        //private ICommand parseNode;
 
-        public ICommand ParseNode
-        {
-            get => parseNode ?? (parseNode = new CommunityToolkit.Mvvm.Input.RelayCommand(ParseNodeProcess));
-            set => parseNode = value;
-        }
+        //public ICommand ParseNode
+        //{
+        //    get => parseNode ?? (parseNode = new CommunityToolkit.Mvvm.Input.RelayCommand(ParseNodeProcess));
+        //    set => parseNode = value;
+        //}
 
         private ICommand downLoadCheckAll;
 
@@ -151,123 +156,107 @@ namespace XHS.Spider.ViewModels
         /// <summary>
         /// 解析下载资源
         /// </summary>
-        /// <param name="nodes"></param>
-        /// <param name="isDownLoadAll"></param>
-        /// <returns></returns>
-        private async void ParseNodeProcess()
+        /// <param name="noteId"></param>
+        private async void ParseNodeProcess(string noteId)
         {
-            _snackbarService.Show("提示", "开始解析", SymbolRegular.ErrorCircle12, ControlAppearance.Success);
-            //TODO:有勾选项解析勾选项无勾选项解析所有笔记
-            IEnumerable<NoteModel> nodes = null;
-            var cheackNodes = this.Nodes.Where(e => e.IsDownLoad == true && e.IsParse == false);
-            if (cheackNodes.Any())
-            {
-                nodes = cheackNodes;
-            }
-            else
-            {
-                nodes = this.Nodes.Where(e => e.IsParse == false);
-            }
+            //_snackbarService.Show("提示", "开始解析", SymbolRegular.ErrorCircle12, ControlAppearance.Success);
+            ////TODO:有勾选项解析勾选项无勾选项解析所有笔记
+            //IEnumerable<NoteModel> nodes = null;
+            //var cheackNodes = this.Nodes.Where(e => e.IsDownLoad == true && e.IsParse == false);
+            //if (cheackNodes.Any())
+            //{
+            //    nodes = cheackNodes;
+            //}
+            //else
+            //{
+            //    nodes = this.Nodes.Where(e => e.IsParse == false);
+            //}
 
             string dirName = Format.FormatFileName(UserInfo.BasicInfo.NickName);
             string dirPath = $"{AppDomain.CurrentDomain.BaseDirectory}DownLoad\\{dirName}";
             //循环笔记数据
-            foreach (var item in nodes)
+            List<DownloadItem> downloadItems = new List<DownloadItem>();
+            var resultData = await _xhsSpiderService.GetNodeDetail(noteId);
+            if (resultData != null && resultData.Success)
             {
-                List<DownloadItem> downloadItems = new List<DownloadItem>();
-                var resultData = await _xhsSpiderService.GetNodeDetail(item.NoteId);
-                if (resultData != null && resultData.Success)
+                var nodeDetail = resultData.Data.Items;
+                foreach (var detailItem in nodeDetail)
                 {
-                    var nodeDetail = resultData.Data.Items;
-                    foreach (var detailItem in nodeDetail)
+                    var nodeCard = detailItem.NoteCard;
+                    var title = Format.FormatFileName(detailItem.NoteCard.Title);
+                    switch (nodeCard.Type)
                     {
-                        var nodeCard = detailItem.NoteCard;
-                        var title = Format.FormatFileName(detailItem.NoteCard.Title);
-                        switch (nodeCard.Type)
-                        {
-                            case "normal":
-                                for (int i = 0; i < nodeCard.ImageList.Count; i++)
+                        case "normal":
+                            for (int i = 0; i < nodeCard.ImageList.Count; i++)
+                            {
+                                var imageUrl = string.Format(BaseImageUrl, nodeCard.ImageList[i].TraceId);
+                                var fpath = $"{dirPath}\\{title}\\{title}-{Guid.NewGuid().ToString()}.png";
+                                DownloadItem downloadImageItem = new DownloadItem()
                                 {
-                                    var imageUrl = string.Format(BaseImageUrl, nodeCard.ImageList[i].TraceId);
-                                    var fpath = $"{dirPath}\\{title}\\{title}-{Guid.NewGuid().ToString()}.png";
-                                    DownloadItem downloadImageItem = new DownloadItem()
-                                    {
-                                        Url = imageUrl,
-                                        FileName = fpath,
-                                        Title = title,
-                                        FolderPath = $"{dirPath}\\{title}",
-                                        Status = DownloadStatus.None,
-                                        FileCount = nodeCard.ImageList.Count,
-                                    };
-                                    downloadItems.Add(downloadImageItem);
-                                }
-
-                                break;
-                            case "video":
-                                var videoUrl = string.Format(BaseVideoUrl, nodeCard.Video.Consumer.OriginVideoKey);
-                                var filePath = $"{dirPath}\\{title}\\{title}-{Guid.NewGuid().ToString()}.mov";
-                                DownloadItem downloadItem = new DownloadItem()
-                                {
-                                    Url = videoUrl,
-                                    FileName = filePath,
+                                    Url = imageUrl,
+                                    FileName = fpath,
                                     Title = title,
                                     FolderPath = $"{dirPath}\\{title}",
                                     Status = DownloadStatus.None,
-                                    FileCount = 1,
+                                    FileCount = nodeCard.ImageList.Count,
                                 };
-                                downloadItems.Add(downloadItem);
-                                break;
-                            default:
-                                break;
-                        }
+                                downloadItems.Add(downloadImageItem);
+                            }
+
+                            break;
+                        case "video":
+                            var videoUrl = string.Format(BaseVideoUrl, nodeCard.Video.Consumer.OriginVideoKey);
+                            var filePath = $"{dirPath}\\{title}\\{title}-{Guid.NewGuid().ToString()}.mov";
+                            DownloadItem downloadItem = new DownloadItem()
+                            {
+                                Url = videoUrl,
+                                FileName = filePath,
+                                Title = title,
+                                FolderPath = $"{dirPath}\\{title}",
+                                Status = DownloadStatus.None,
+                                FileCount = 1,
+                            };
+                            downloadItems.Add(downloadItem);
+                            break;
+                        default:
+                            break;
                     }
-                    var nodeEntity = this.Nodes.FirstOrDefault(e => e.NoteId == item.NoteId);
-                    if (nodeEntity != null)
-                    {
-                        nodeEntity.IsParse = true;
-                        nodeEntity.FileCount = downloadItems.Count();
-                        nodeEntity.DownloadItems = downloadItems;
-                        nodeEntity.IsNormal = true;
-                    }
-                    this.ParseNodeCount = $"已解析({this.Nodes.Where(e => e.IsParse == true).Count()})条";
+                }
+                var nodeEntity = this.Nodes.FirstOrDefault(e => e.NoteId == noteId);
+                if (nodeEntity != null)
+                {
+                    nodeEntity.IsParse = true;
+                    nodeEntity.FileCount = downloadItems.Count();
+                    nodeEntity.DownloadItems = downloadItems;
+                    nodeEntity.IsNormal = true;
+                }
+            }
+            else
+            {
+                //网络连接异常，请检查网络设置或重启试试
+                if (resultData.Code == 300012)
+                {
+                    _notifyIcon.ShowBalloonTip($"解析失败，接口异常。已完成解析【{this.Nodes.Where(e => e.IsParse == true).Count()}】条笔记", "提示", BalloonIcon.Error);
                 }
                 else
                 {
-                    //网络连接异常，请检查网络设置或重启试试
-                    if (resultData.Code == 300012)
+                    //-510001  笔记状态异常，请稍后查看
+                    if (resultData.Code == -510001)
                     {
-                        _notifyIcon.ShowBalloonTip($"解析失败，接口异常。已完成解析【{this.Nodes.Where(e => e.IsParse == true).Count()}】条笔记", "提示", BalloonIcon.Error);
-                        break;
-                    }
-                    else
-                    {
-                        //-510001  笔记状态异常，请稍后查看
-                        if (resultData.Code == -510001)
+                        //TODO:其他异常，将笔记状态改为异常
+                        var nodeEntity = this.Nodes.FirstOrDefault(e => e.NoteId == noteId);
+                        if (nodeEntity != null)
                         {
-                            //TODO:其他异常，将笔记状态改为异常
-                            var nodeEntity = this.Nodes.FirstOrDefault(e => e.NoteId == item.NoteId);
-                            if (nodeEntity != null)
-                            {
-                                nodeEntity.IsParse = true;
-                                nodeEntity.FileCount = 0;
-                                nodeEntity.DownloadItems = downloadItems;
-                                nodeEntity.IsNormal = false;
-                            }
-                            this.ParseNodeCount = $"已解析({this.Nodes.Where(e => e.IsParse == true).Count()})条";
+                            nodeEntity.IsParse = true;
+                            nodeEntity.FileCount = 0;
+                            nodeEntity.DownloadItems = downloadItems;
+                            nodeEntity.IsNormal = false;
                         }
                     }
-
                 }
-                Random ran = new Random();
-                int awaitTime = ran.Next(2800, 3500);
-                await Task.Delay(awaitTime);
-                this.DataGridItemCollection = this.Nodes.ToArray();
+
             }
-            var parseNodes = nodes.Where(e => e.IsParse == false);
-            if (parseNodes.Count() == 0)
-            {
-                _notifyIcon.ShowBalloonTip($"解析完成", "提示", BalloonIcon.Info);
-            }
+            this.DataGridItemCollection = this.Nodes.ToArray();
         }
 
         /// <summary>
@@ -299,8 +288,45 @@ namespace XHS.Spider.ViewModels
         /// <summary>
         /// 初始化数据
         /// </summary>
-        public override void ExecuteInitData()
+        public async override void ExecuteInitData()
         {
+            if (!string.IsNullOrEmpty(InputText))
+            {
+                if (InputText.Contains("user/profile/"))
+                {
+                    var id = SearchService.GetId(InputText, BaseUrl);
+                    if (string.IsNullOrEmpty(id))
+                    {
+                        Logger.Error($"URL有误：{InputText}");
+                        return;
+                    }
+                    else
+                    {
+                        var apiResult = await _xhsSpiderService.GetOtherInfo(id);
+                        if (apiResult != null && apiResult.Success)
+                        {
+                            UserInfo = apiResult.Data;
+                           
+                            var nodes = await _xhsSpiderService.GetAllUserNode(id);
+                            foreach (var node in nodes)
+                            {
+                                node.LikedCount = node.interact_info?.LikedCount;
+                            }
+                            Nodes = nodes.ToArray();
+                            NoteCount = $"({nodes.Count()})条";
+                            DataGridItemCollection = Nodes;
+                        }
+                        else
+                        {
+                            _snackbarService.Show("异常", apiResult?.Msg, SymbolRegular.ErrorCircle12, ControlAppearance.Danger);
+                        }
+                    }
+                }
+                else
+                {
+                    _snackbarService.Show("提示", "当前Url不符合所属模块搜索要求", SymbolRegular.ErrorCircle12, ControlAppearance.Danger);
+                }
+            }
         }
 
         public void OnNavigatedFrom()
