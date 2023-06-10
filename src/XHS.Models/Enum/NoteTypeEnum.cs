c52735aa1c0874582eb2141b3175085d3a253f08
初十 <392706283@qq.com>
2023-06-10 08:55:45 +0800

    增加枚举

---
+++ src/XHS.Models/Enum/NoteTypeEnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHS.Models.Enum
+{
+    /// <summary>
+    /// 笔记类型
+    /// </summary>
+    public enum NoteTypeEnum
+    {
+        /// <summary>
+        /// 笔记
+        /// </summary>
+        UserPosted=1,
+        /// <summary>
+        /// 收藏
+        /// </summary>
+        Collect=2,
+        /// <summary>
+        /// 点赞
+        /// </summary>
+        Like=3,
+    }
+}
